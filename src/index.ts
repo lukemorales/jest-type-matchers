db6a8e432eda632f6793b65c3496b43595fd8b30
Luke Morales <lukemorales@live.com>
2022-10-08 16:56:52 -0300

    style: lint files

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { Equal, NotEqual } from "./types";
+import { Equal, NotEqual } from './types';
 
 const matcherResult = (message: string): jest.CustomMatcherResult => ({
   pass: true,
   message: () => message,
-})
+});
 
 const customMathers: jest.ExpectExtendMap = {
   toHaveType(received: unknown) {
-    return matcherResult(`${received} does not match the expected types`)
+    return matcherResult(`${received} does not match the expected types`);
   },
   toNotHaveType(received: unknown) {
-    return matcherResult(`${received} matches the expected types`)
+    return matcherResult(`${received} matches the expected types`);
   },
-}
+};
 
 expect.extend(customMathers);
 
@@ -23,7 +23,7 @@ expect.extend(customMathers);
  * Note that these matchers will never fail in the test suite,
  * they can only fail in type land.
  */
- export interface TypeMatchers<R, X> {
+export interface TypeMatchers<R, X> {
   /**
    * Assert that the received variable has the expected type.
    *
@@ -32,11 +32,11 @@ expect.extend(customMathers);
    */
   toHaveType<Y extends Equal<X, Y> extends true ? X : never>(): R;
   /**
-  * Assert that the received variable does not have the expected type.
-  *
-  * @example
-  * expect('hello world').toNotHaveType<number>()
-  */
+   * Assert that the received variable does not have the expected type.
+   *
+   * @example
+   * expect('hello world').toNotHaveType<number>()
+   */
   toNotHaveType<Y extends NotEqual<X, Y> extends true ? any : never>(): R;
 }
 
--- src/types.ts
+++ src/types.ts
@@ -4,5 +4,5 @@
  * @see {@link https://github.com/total-typescript/beginners-typescript-tutorial/blob/main/src/helpers/type-utils.ts for original types}
  */
 
- export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
- export type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
+export type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
